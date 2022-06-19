6c6a832bd5aba87be0fd17bf683dd43ca26ae0ae
osifojohn <osifojohntec@gmail.com>
2022-06-19 17:38:13 +0100

    Updates

---
+++ precache-manifest.6d322859468da4058c5662b009c1e1eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f47f3e2cac0954c2fe09e6cc7bc68e07",
+    "url": "/ReactJs-TodoApp1/index.html"
+  },
+  {
+    "revision": "41f33a99d267404af04e",
+    "url": "/ReactJs-TodoApp1/static/css/main.1d1281d5.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/ReactJs-TodoApp1/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJs-TodoApp1/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f33a99d267404af04e",
+    "url": "/ReactJs-TodoApp1/static/js/main.4229bcd1.chunk.js"
+  },
+  {
+    "revision": "dc919a1e6a68b699c310",
+    "url": "/ReactJs-TodoApp1/static/js/runtime-main.225b70c1.js"
+  }
+]);
